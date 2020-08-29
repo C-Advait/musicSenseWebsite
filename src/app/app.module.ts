4ab4cc19f02b4cc3cc68ab099660e5b31cbc31af
C-Advait <advait.c123@gmail.com>
2020-08-29 15:22:35 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AnalysisComponent } from './analysis/analysis.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnalysisComponent
   ],
   imports: [
     BrowserModule,
